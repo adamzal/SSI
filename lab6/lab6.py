4918e2aa9fe9418803b5170fa78184d235c92c7a
adamzal <adam.zalewski1999@o2.pl>
2023-10-17 17:40:28 +0200

    add txt files and read_data function

--- lab6/lab6.py
+++ lab6/lab6.py
@@ -1,54 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-z_w_1 = np.array([
-    [0, 0, 0, 1],
-    [0, 0, 1, 1],
-    [0, 1, 0, 1],
-    [0, 0, 0, 1],
-    [0, 0, 0, 1]
-])
-
-z_w_2 = np.array([
-    [0, 1, 1, 1],
-    [1, 0, 0, 1],
-    [0, 0, 1, 0],
-    [0, 1, 0, 0],
-    [1, 1, 1, 1]
-])
-
-z_w_3 = np.array([
-    [1, 1, 1, 0],
-    [0, 0, 0, 1],
-    [1, 1, 1, 1],
-    [0, 0, 0, 1],
-    [1, 1, 1, 0]
-])
-
-z_t_1 = np.array([
-    [0, 0, 0, 0],
-    [0, 0, 1, 1],
-    [0, 1, 1, 1],
-    [0, 0, 0, 1],
-    [0, 0, 0, 1]
-])
-
-z_t_2 = np.array([
-    [1, 1, 1, 1],
-    [0, 0, 0, 1],
-    [1, 1, 1, 1],
-    [0, 0, 1, 1],
-    [1, 1, 1, 1]
-])
-
-z_t_3 = np.array([
-    [1, 1, 1, 1],
-    [0, 0, 0, 1],
-    [0, 0, 1, 0],
-    [1, 1, 0, 0],
-    [1, 1, 1, 1]
-])
-
 
 def euklides(x0, x1, y0, y1) -> np.core.numeric:
     return np.sqrt((x0 - x1) ** 2 + (y0 - y1) ** 2)
@@ -76,23 +28,29 @@ def miara_niepodobienstwa(ba, bb) -> np.core.numeric:
 def miara_podobienstwa_obustronnego(ba, bb) -> np.core.numeric:
     return -(miara_niepodobienstwa(ba, bb) + miara_niepodobienstwa(bb, ba))
 
+def read_data(file_name):
+    with open(file_name, "r") as file:
+        data = file.read().split("\n\n")
 
-print(miara_niepodobienstwa(z_t_2, z_w_1))
-print(miara_niepodobienstwa(z_w_1, z_t_2))
-print(miara_podobienstwa_obustronnego(z_w_1, z_t_2))
+    main_tab = []
+    for matrix in data:
+        tab = np.array([list(map(int, row.split())) for row in matrix.strip().split('\n')])
+        main_tab.append(tab)
 
-template_bitmaps = [z_w_1, z_w_2, z_w_3]
-test_bitmaps = [z_t_1, z_t_2, z_t_3]
+    return main_tab
+
+patterns = read_data("patterns.txt")
+test = read_data("test.txt")
 
 miary = []
-for i in test_bitmaps:
+for i in test:
     m = []
-    for j in template_bitmaps:
+    for j in patterns:
         m += [miara_podobienstwa_obustronnego(i, j)]
 
     fig, ax = plt.subplots(1, 2)
     ax[0].imshow(i, cmap='gray_r')
     ax[0].set_title('Bitmapa testowa')
-    ax[1].imshow(template_bitmaps[np.argmax(m)], cmap='gray_r')
+    ax[1].imshow(patterns[np.argmax(m)], cmap='gray_r')
     ax[1].set_title('Bitmapa wzorcowa')
     plt.show()
